17e2a98e00abd0575c5cedcb1787184904ebea8e
Krzysztof Kończak <krzysztof10konczak@wp.pl>
2021-02-16 22:37:31 +0100

    Add ReactiveUI library

---
+++ TeaMan/TeaMan/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.Diagnostics;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace TeaMan.ViewModels
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public MainWindowViewModel()
+        {
+            Command1 = ReactiveCommand.Create(Command1Impl);
+
+            this.WhenAnyValue(e => e.Pole1)
+                .Where(e => !string.IsNullOrEmpty(e) && e.Length % 2 == 0)
+                .Select(e => Unit.Default)
+                .InvokeCommand(Command1);
+        }
+
+        [Reactive]
+        public string Pole1 { get; set; }
+
+        public ReactiveCommand<Unit, Unit> Command1 { get; }
+
+        private void Command1Impl()
+        {
+            Debug.WriteLine("test");
+        }
+    }
+}
---
+++ TeaMan/TeaMan/Views/MainWindowView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using TeaMan.ViewModels;
+
+namespace TeaMan.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}

3d90e8228bf744819fff8cd6ad38edaf8c4c30c0
Krzysztof Kończak <krzysztof10konczak@wp.pl>
2021-02-16 22:37:31 +0100

    Add ReactiveUI library

--- TeaMan/TeaMan/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ TeaMan/TeaMan/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace TeaMan {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TeaMan;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TeaMan;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

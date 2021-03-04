f38453426e5c227ed4748d7ce2d240d028475c48
Krzysztof Kończak <krzysztof10konczak@wp.pl>
2021-03-05 00:10:36 +0100

    #11 SQLLite database model

---
+++ TeaMan/TeaMan/DatabaseContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeaMan.Models;
+
+namespace TeaMan
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Calendar> Calendars { get; set; }
+        public DbSet<UserTask> Tasks { get; set; }
+        public DbSet<TaskStatus> TaskStatuses { get; set; }
+        public DbSet<TaskType> TaskTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlite($"Data Source=database.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTask>().HasOne(e => e.Calendar)
+                .WithMany(e => e.Tasks)
+                .HasForeignKey(e => e.CalendarId);
+
+            modelBuilder.Entity<UserTask>().HasOne(e => e.TaskStatus)
+                .WithMany(e => e.Tasks)
+                .HasForeignKey(e => e.TaskStatusId);
+
+            modelBuilder.Entity<UserTask>().HasOne(e => e.TaskType)
+                .WithMany(e => e.Tasks)
+                .HasForeignKey(e => e.TaskTypeId);
+
+            modelBuilder.Entity<TaskStatus>().HasOne(e => e.Calendar)
+                .WithMany(e => e.TaskStatuses)
+                .HasForeignKey(e => e.CalendarId);
+
+            modelBuilder.Entity<TaskType>().HasOne(e => e.Calendar)
+                .WithMany(e => e.TaskTypes)
+                .HasForeignKey(e => e.CalendarId);
+        }
+    }
+}

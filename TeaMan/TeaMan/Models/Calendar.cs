501069eab0281a386f4b0cc9d297ae0b470357e8
Krzysztof Kończak <krzysztof10konczak@wp.pl>
2022-01-21 22:55:25 +0100

    #23 Create separate class for all Database Actions

--- TeaMan/TeaMan/Models/Calendar.cs
+++ TeaMan/TeaMan/Models/Calendar.cs
@@ -4,8 +4,14 @@ namespace TeaMan.Models
 {
     public class Calendar : OrderedListItemEntity
     {
+        public Calendar(string name, int order) : base(name, order) { }
+
+        public Calendar() { }
+
         public ICollection<UserTask> UserTasks { get; set; } = new List<UserTask>();
+
         public ICollection<TaskStatus> TaskStatuses { get; set; } = new List<TaskStatus>();
+
         public ICollection<TaskType> TaskTypes { get; set; } = new List<TaskType>();
     }
 }
--- TeaMan/TeaMan/Models/OrderedListItemEntity.cs
+++ TeaMan/TeaMan/Models/OrderedListItemEntity.cs
@@ -2,6 +2,10 @@
 {
     public class OrderedListItemEntity : Entity
     {
+        public OrderedListItemEntity(string name, int order) => (Name, Order) = (name, order);
+
+        public OrderedListItemEntity() { }
+
         public string Name { get; set; }
 
         public int Order { get; set; }

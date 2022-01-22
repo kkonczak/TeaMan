15737948b29ec6134556a9c85988508c53fdb1e5
Krzysztof Kończak <krzysztof10konczak@wp.pl>
2022-01-22 18:05:23 +0100

    Add "(all)" option to comboboxes "TaskStatus" and "TaskType"

--- TeaMan/TeaMan/Controls/CustomComboBox.cs
+++ TeaMan/TeaMan/Controls/CustomComboBox.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -10,10 +11,32 @@ namespace TeaMan.Controls
             DependencyProperty.Register(nameof(AddButtonCommand), typeof(ICommand), typeof(CustomComboBox),
                 new PropertyMetadata(default));
 
+        private static readonly DependencyPropertyKey ClearButtonCommandPropertyKey =
+            DependencyProperty.RegisterReadOnly(nameof(ClearButtonCommand), typeof(ICommand), typeof(CustomComboBox),
+                new FrameworkPropertyMetadata(default));
+
+        public static readonly DependencyProperty ClearButtonCommandProperty = ClearButtonCommandPropertyKey.DependencyProperty;
+
         public ICommand AddButtonCommand
         {
             get { return (ICommand)GetValue(AddButtonCommandProperty); }
             set { SetValue(AddButtonCommandProperty, value); }
+        }
+
+        public ICommand ClearButtonCommand
+        {
+            get { return (ICommand)GetValue(ClearButtonCommandProperty); }
+            protected set { SetValue(ClearButtonCommandPropertyKey, value); }
+        }
+
+        public CustomComboBox()
+            : base()
+        {
+            ClearButtonCommand = new DelegateCommand(() =>
+                {
+                    SelectedItem = null;
+                    IsDropDownOpen = false;
+                });
         }
     }
 }
